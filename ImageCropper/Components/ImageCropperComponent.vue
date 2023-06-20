8f5a144b2fb2d5d52d669cf20c680e7e3b958488
Tom Schwiha <tom@schwiha.de>
2023-06-20 12:04:08 +0200

    Initial commit

---
+++ ImageCropper/Components/ImageCropperComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="app-view">
+        <view-view></view-view>
+        <flash-messages></flash-messages>
+        <action-bar></action-bar>
+        <image-cropper></image-cropper>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import '@enhavo/app/assets/styles/view.scss';
+
+@Component()
+export default class ImageCropperComponent extends Vue
+{
+    mounted() {
+        $(document).on('change', ':input', () => {
+            this.format.changed = true;
+        });
+    }
+}
+</script>
+
+<style lang="css">
+    .app-view { height: 100vh }
+</style>
---
+++ MediaLibrary/Components/ApplicationComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="app-view">
+        <view-view></view-view>
+        <flash-messages></flash-messages>
+        <action-bar></action-bar>
+        <media-library></media-library>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import '@enhavo/app/assets/styles/view.scss';
+import '@enhavo/media/assets/styles/_library.scss';
+
+@Component()
+export default class ApplicationComponent extends Vue
+{
+
+}
+</script>
+
+<style lang="css">
+    .app-view { height: 100vh }
+</style>
